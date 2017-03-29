140b2541d34efaad7d441fae555f392b75b88d1c
Jenn <Jenn>
2017-03-29 16:43:08 -0400

    changed color, note clean up

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 //this is a custom JS //
 
-//when the "join-now" button is clicked the form will hide and the hidden message of "THANK YOU!"
+//when the "join-now" button is clicked the form will hide, and the hidden message of "THANK YOU!"
 //will appear
 
 $('#button').click(function() {
